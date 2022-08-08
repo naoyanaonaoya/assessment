d3b46fc48608ca2aed47bedd9cc58ce186eee1f7
naoyanaonaoya <naoya.basket.a.i@gmail.com>
2022-08-08 14:20:21 +0900

    add

--- assessment.js
+++ assessment.js
@@ -20,13 +20,27 @@ assessmentButton.onclick = () => {
 
   // TODO: 診断結果表示エリアの作成
   resultDivided.innerText = "";
-  const header = document.createElement("h3");
-  header.innerText = "診断結果";
-  resultDivided.appendChild(header);
+  // headerDividedの作成
+  const headerDivided = document.createElement("div");
+  headerDivided.setAttribute("class", "card-header");
+  headerDivided.innerText = "診断結果";
+  // body dividedの作成
+  const bodyDivided = document.createElement("div");
+  bodyDivided.setAttribute("class", "card-body");
+
   const paragraph = document.createElement("p");
+  paragraph.setAttribute("class", "card-text");
   const result = assessment(userName);
   paragraph.innerText = result;
-  resultDivided.appendChild(paragraph);
+  bodyDivided.appendChild(paragraph);
+
+  // resultDividedにBootstrapのスタイルを適用する
+  resultDivided.setAttribute("class", "card");
+  resultDivided.setAttribute("style", "max-width: 700px");
+
+  // headerDividedとbodyDivideをresultDividedに差し込む
+  resultDivided.appendChild(headerDivided);
+  resultDivided.appendChild(bodyDivided);
 
   // TODO: ツイートエリアの作成
   tweetDivided.innerText = "";
